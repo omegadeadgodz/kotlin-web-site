d02c9e603b7a2e97770e7f7aecc895a8cf9731f0
zoobestik <kb.chernenko@gmail.com>
2021-05-06 16:34:26 +0300

    fixup! tmp

--- @ktl-components/compile.js
+++ @ktl-components/compile.js
@@ -2,20 +2,6 @@
 const babelRc = Object.assign({}, require('./.babelrc.json'), {
     cache: true,
     extensions: ['.mjs', '.js', '.jsx'],
-    "plugins": [
-        [
-            "module-resolver",
-            {
-                "root": ["."],
-                "alias": {
-                    "react": "preact/compat",
-                    "react-dom": "preact/compat",
-                    'create-react-class': 'preact-compat/lib/create-react-class',
-                    'react-dom-factories': 'preact-compat/lib/react-dom-factories'
-                }
-            }
-        ]
-    ]
 });
 
 require('@babel/register')(babelRc);
